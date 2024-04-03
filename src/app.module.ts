20f4bc618bb9cf5c41275f6847d3588e5e3f9e2e
CarlosJaBr <carjabo12@gmail.com>
2024-04-03 09:41:32 -0500

    add typeorm and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
 import { SeedModule } from './seed/seed.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [CarsModule, BrandsModule, SeedModule],
+
+  //Esto se hace para hacerlo de manera local. Cuando
+  //se hace el despligue, las variables de env se ajustan para que funcione.
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CarsModule, BrandsModule, SeedModule],
   controllers: [AppController],
   providers: [AppService],
 })
